fe45912abc00e1af00d84813f00092bcd009c642
Sajna N Shetty <sajna.shetty@gmail.com>
2020-06-17 18:04:05 +0530

    support travis initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='python-learning',
+      version='0.0.1',
+      description='Advanced python learning concepts',
+      author='Sajna',
+      author_email='',
+      url='',
+      packages=find_packages(),
+     )
