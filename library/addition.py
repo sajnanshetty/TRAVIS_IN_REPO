4bf50afeab94071e0deac16e94679d9b57894237
Sajna N Shetty <sajna.shetty@gmail.com>
2020-06-17 16:36:56 +0530

    initial commit

---
+++ library/addition.py
@@ -0,0 +1,9 @@
+import sys
+
+def abc(a, b):
+    # initializing data to be stored in db
+    return a + b
+
+
+if __name__ == '__main__':
+    abc(10, 1)
---
+++ test/units/library/test_addition.py
@@ -0,0 +1,7 @@
+from library.addition import *
+
+print(abc(1, 2))
+
+def test_abc():
+    val = abc(1, 2)
+    assert val == 3
